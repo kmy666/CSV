55a6931be663105d6d6357c66bb81b235b5366ea
kmy666 <53104501+kmy666@users.noreply.github.com>
2026-06-24 19:19:33 -0500

    Update uno.py

--- uno.py
+++ uno.py
@@ -1,3 +1,5 @@
+import streamlit as st
+import pandas as pd
 import requests
 
 API_KEY = "2P6SoRwyMPoXyddzmrzR6erv9Wdrlla2O7iVEQt6"
@@ -7,7 +9,14 @@ url = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}"
 response = requests.get(url)
 data = response.json()
 
-print("Título:", data["title"])
-print("Descripción:", data["explanation"])
-print("Imagen:", data["url"])
+df = pd.DataFrame([{
+    "titulo": data["title"],
+    "fecha": data["date"],
+    "explicacion": data["explanation"]
+}])
 
+st.title("NASA API")
+
+st.dataframe(df)
+
+st.image(data["url"])
