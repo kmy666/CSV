9fa850163296f2dff1c87fc45f4dc1fc4bca46e8
kmy666 <53104501+kmy666@users.noreply.github.com>
2026-06-24 19:04:44 -0500

    Update uno.py

--- uno.py
+++ uno.py
@@ -1,1 +1,13 @@
+import requests
+
+API_KEY = "2P6SoRwyMPoXyddzmrzR6erv9Wdrlla2O7iVEQt6"
+
+url = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}"
+
+response = requests.get(url)
+data = response.json()
+
+print("Título:", data["title"])
+print("Descripción:", data["explanation"])
+print("Imagen:", data["url"])
 
